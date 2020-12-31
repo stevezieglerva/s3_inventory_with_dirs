eb7d2abba319f564acd912044883c7729353a961
Steve Ziegler <stephen.v.ziegler@gmail.com>
2020-12-31 12:13:24 -0500

    working with 10 folders

--- S3Inventory.py
+++ S3Inventory.py
@@ -2,7 +2,10 @@ from S3 import *
 from collections import namedtuple
 
 
-AthenaS3Object = namedtuple("AthenaS3Object", "bucket key date size parent1")
+AthenaS3Object = namedtuple(
+    "AthenaS3Object",
+    "bucket key date size parent1 parent2 parent3 parent4 parent5 parent6 parent7 parent8 parent9 parent10",
+)
 
 
 class S3Inventory:
@@ -18,13 +21,35 @@ class S3Inventory:
 
     def format_for_athena(self, s3_objects):
         results = []
+        parent_options = [f"parent{i}" for i in range(1, 11)]
+        empty_parent_values = {}
+        for parent in parent_options:
+            empty_parent_values[parent] = ""
+
         for object in s3_objects:
+            parent_values = empty_parent_values
+            folders = object.key.split("/")[:-1]
+            for count, folder in enumerate(folders):
+                index_number = count + 1
+                index = f"parent{index_number}"
+                parent_values[index] = folder
+            print(parent_values)
+
             new_athena = AthenaS3Object(
                 bucket=object.bucket,
                 key=object.key,
                 date=object.date,
                 size=object.size,
-                parent1="",
+                parent1=parent_values["parent1"],
+                parent2=parent_values["parent2"],
+                parent3=parent_values["parent3"],
+                parent4=parent_values["parent4"],
+                parent5=parent_values["parent5"],
+                parent6=parent_values["parent6"],
+                parent7=parent_values["parent7"],
+                parent8=parent_values["parent8"],
+                parent9=parent_values["parent9"],
+                parent10=parent_values["parent10"],
             )
             results.append(new_athena)
         return results
--- tests/test_unit_S3Inventory.py
+++ tests/test_unit_S3Inventory.py
@@ -2,7 +2,7 @@ import unittest
 from unittest import mock
 from unittest.mock import patch, Mock, MagicMock, PropertyMock
 from S3 import *
-from S3Inventory import S3Inventory
+from S3Inventory import S3Inventory, AthenaS3Object
 
 
 class S3InventoryUnitTests(unittest.TestCase):
@@ -20,12 +20,14 @@ class S3InventoryUnitTests(unittest.TestCase):
         # Assert
         self.assertGreater(len(results), 0)
 
-    def test_format_for_athena__given_some_files__then_results_formatted_correctly(
+    def test_format_for_athena__given_one_file__then_results_formatted_correctly(
         self,
     ):
         # Arrange
         input = [
-            S3Object(bucket="fake-bucket", key="object_1", date="2020-01-01", size=100)
+            S3Object(
+                bucket="fake-bucket", key="dir1/object_1", date="2020-01-01", size=100
+            )
         ]
         subject = S3Inventory("fake-bucket", S3FakeLocal)
 
@@ -34,6 +36,25 @@ class S3InventoryUnitTests(unittest.TestCase):
 
         # Assert
         self.assertGreater(len(results), 0)
+        expected = AthenaS3Object(
+            bucket=input[0].bucket,
+            key=input[0].key,
+            date=input[0].date,
+            size=input[0].size,
+            parent1="dir1",
+            parent2="",
+            parent3="",
+            parent4="",
+            parent5="",
+            parent6="",
+            parent7="",
+            parent8="",
+            parent9="",
+            parent10="",
+        )
+        print(results[0])
+        print(expected)
+        self.assertEqual(results[0], expected)
 
 
 if __name__ == "__main__":
