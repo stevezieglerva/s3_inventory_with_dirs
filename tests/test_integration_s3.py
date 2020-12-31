f8c0f4d89173faf3e322c832e71d9a1c77729357
Steve Ziegler <stephen.v.ziegler@gmail.com>
2020-12-31 09:31:16 -0500

    basic s3 tests fixed

--- tests/test_integration_s3.py
+++ tests/test_integration_s3.py
@@ -4,13 +4,27 @@ from S3 import S3
 
 
 class S3IntegrationTests(unittest.TestCase):
-    def test_list_objects__given_valid_bucket__then_list_returned(self):
+    def test_list_objects__given_valid_bucket_10_max__then_list_returned(self):
         # Arrange
         subject = S3()
 
         # Act
+        results = subject.list_objects("svz-master-pictures-new", "failed", 10)
 
         # Assert
+        self.assertEqual(len(results), 10)
+        print(results[0])
+
+    def test_list_objects__given_valid_bucket_no_max__then_list_returned(self):
+        # Arrange
+        subject = S3()
+
+        # Act
+        results = subject.list_objects("svz-master-pictures-new", "failed")
+
+        # Assert
+        self.assertGreater(len(results), 10)
+        print(results[0])
 
 
 if __name__ == "__main__":
