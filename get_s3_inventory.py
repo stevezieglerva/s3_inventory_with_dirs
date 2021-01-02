91f8bc1c888bfb01dd5920dfdda2bb348dec52c4
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-01-02 18:20:27 -0500

    updated main run.sh file

--- get_s3_inventory.py
+++ get_s3_inventory.py
@@ -4,4 +4,4 @@ from S3Inventory import S3Inventory, WriteResults
 # Get the full inventory of S3 objects
 subject = S3Inventory("svz-master-pictures-new", S3())
 results = subject.create_inventory("svz-master-pictures-new", "inventory", "")
-assert results.line_count >= 100000, "Expected to find more than 100,000 objects"
+assert results.line_count >= 65000, "Expected to find more than 100,000 objects"
--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
+# Main script to create and download the S3 file inventory
 python3 get_s3_inventory.py  
+aws s3 cp s3://svz-master-pictures-new/inventory/inventory.csv .
