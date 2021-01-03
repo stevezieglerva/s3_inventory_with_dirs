e438befe0c1f024623c71aa84773f2e59c439c66
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-01-03 09:16:25 -0500

    reverse the external drive backup to go back in time

--- sync_external_to_s3/sync_external_to_s3.sh
+++ sync_external_to_s3/sync_external_to_s3.sh
@@ -1,10 +1,10 @@
 
-for i in {2003..2020}
+for i in $(seq 2020 -1 2003) #{2003..2020}
 do
     echo ""
     echo "************"
     echo "   $i"
-    /usr/local/bin/aws s3 sync /Volumes/My\ Passport/ZieglerPics/$i s3://svz-master-pictures/raw-photos/$i 
+    /usr/local/bin/aws s3 sync /Volumes/My\ Passport/ZieglerPics/$i s3://svz-master-pictures-new/raw-photos/$i 
 done
 
 
