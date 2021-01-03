cd45c2dfa58636b2080315886b7b03d6fc2109be
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-01-03 09:33:01 -0500

    add year check to run

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 # Main script to create and download the S3 file inventory
 python3 get_s3_inventory.py  
 aws s3 cp s3://svz-master-pictures-new/inventory/inventory.csv .
+
+# Check the various count
+grep -E "2012" inventory.csv | wc -l
+grep -E "2018" inventory.csv | wc -l
