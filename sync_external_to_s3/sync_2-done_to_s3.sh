cb73da6c274a9c5db6dee6f1e65527afc9314879
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-01-16 12:01:10 -0500

    add new bash script to sync 2-done processing folder into raw-photos

---
+++ sync_external_to_s3/sync_2-done_to_s3.sh
@@ -0,0 +1,4 @@
+
+aws s3 sync "/Users/sziegler/Dropbox/ZieglerPics/Master Starred Pictures/2_done/2019/" s3://svz-master-pictures-new/raw-photos/2019
+aws s3 sync "/Users/sziegler/Dropbox/ZieglerPics/Master Starred Pictures/2_done/2020/" s3://svz-master-pictures-new/raw-photos/2020
+aws s3 sync "/Users/sziegler/Dropbox/ZieglerPics/Master Starred Pictures/2_done/2021/" s3://svz-master-pictures-new/raw-photos/2021
