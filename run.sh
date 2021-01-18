631fcbaf524dee2642eef686ab5270165fae6d27
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-01-18 09:07:50 -0500

    updated sync files and reporting

--- run.sh
+++ run.sh
@@ -3,5 +3,23 @@ python3 get_s3_inventory.py
 aws s3 cp s3://svz-master-pictures-new/inventory/inventory.csv .
 
 # Check the various count
-grep -E "2012" inventory.csv | wc -l
-grep -E "2018" inventory.csv | wc -l
+
+export year="2012"
+echo "$year"
+grep -E "$year" inventory.csv | wc -l
+
+export year="2012"
+echo "$year"
+grep -E "$year" inventory.csv | wc -l
+
+export year="2018"
+echo "$year"
+grep -E "$year" inventory.csv | wc -l
+
+export year="2019"
+echo "$year"
+grep -E "$year" inventory.csv | wc -l
+
+export year="2020"
+echo "$year"
+grep -E "$year" inventory.csv | wc -l
--- sync_external_to_s3/sync_2-done_to_s3.sh
+++ sync_external_to_s3/sync_2-done_to_s3.sh
@@ -1,4 +1,17 @@
+echo "2008"
+aws s3 sync "/Users/sziegler/Dropbox/ZieglerPics/Master Starred Pictures/2_done/2008/" s3://svz-master-pictures-new/raw-photos/2008
 
+echo "2017"
+aws s3 sync "/Users/sziegler/Dropbox/ZieglerPics/Master Starred Pictures/2_done/2017/" s3://svz-master-pictures-new/raw-photos/2017
+
+echo "2018"
+aws s3 sync "/Users/sziegler/Dropbox/ZieglerPics/Master Starred Pictures/2_done/2018/" s3://svz-master-pictures-new/raw-photos/2018
+
+echo "2019"
 aws s3 sync "/Users/sziegler/Dropbox/ZieglerPics/Master Starred Pictures/2_done/2019/" s3://svz-master-pictures-new/raw-photos/2019
+
+echo "2020"
 aws s3 sync "/Users/sziegler/Dropbox/ZieglerPics/Master Starred Pictures/2_done/2020/" s3://svz-master-pictures-new/raw-photos/2020
+
+echo "2021"
 aws s3 sync "/Users/sziegler/Dropbox/ZieglerPics/Master Starred Pictures/2_done/2021/" s3://svz-master-pictures-new/raw-photos/2021
