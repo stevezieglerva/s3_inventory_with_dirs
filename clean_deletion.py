7eefc54b1b0a16d284859260d0900fbc4bdae02b
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-01-27 17:41:59 -0500

    remove hardcoded buckets and refactor into cml arguments

--- clean_deletion.py
+++ clean_deletion.py
@@ -3,11 +3,14 @@ import json
 print("*** original files:")
 with open("inventory_pre_delete.csv", "r") as file:
     lines = file.readlines()
-    orig_lines = [l.strip() for l in lines[1:] if "original/2011" in l]
+    orig_lines = [l.strip() for l in lines[1:] if "original/2011/" in l]
+    original_keys = []
     for line in orig_lines:
         fields = line.split(",")
-        key = fields[1].replace('"', "").replace("original/", "")
-        print(key)
+        key = fields[1].replace('"', "").replace("original/2011/", "")
+        original_keys.append(key)
+count = len(original_keys)
+print(f"Original keys: {count}")
 
 print("*** external drive files:")
 external_file_lookup = {}
@@ -18,6 +21,10 @@ with open("external_drive_find_output/all_pictures_drive_a_beatup.txt", "r") as 
         fields = line.split("/")
         filename = fields[-1:][0]
         filename = filename.replace('"', "")
-        print(filename)
         external_file_lookup[filename] = line
-print(json.dumps(external_file_lookup, indent=3, default=str))
+count = len(external_file_lookup)
+print(f"External filenames: {count}")
+
+for file in original_keys:
+    lookup = external_file_lookup[file]
+    print(f"{file} -> {lookup}")
--- get_s3_inventory.py
+++ get_s3_inventory.py
@@ -1,16 +1,24 @@
 from datetime import datetime
 from S3 import *
 from S3Inventory import S3Inventory, WriteResults
+import sys
+
+# Get the command-line args
+assert (
+    len(sys.argv) == 4
+), "Did not receive the expected source bucket, destination bucket, and inventory prefix command line arguments"
+source_bucket = sys.argv[1]
+destination_bucket = sys.argv[2]
+destination_prefix = sys.argv[3]
 
 # Get the full inventory of S3 objects
 start = datetime.now()
 
-subject = S3Inventory("svz-master-pictures-new", S3())
-results = subject.create_inventory("svz-master-pictures-new", "inventory", "")
+subject = S3Inventory(destination_bucket, S3())
+results = subject.create_inventory(source_bucket, destination_prefix, "")
 
 end = datetime.now()
 duration = end - start
-print("Duration:")
-print(duration)
+print(f"Duration: {duration}")
 
-assert results.line_count >= 80000, "Expected to find more than 100,000 objects"
+print(f"Objects found: {results.line_count}")
